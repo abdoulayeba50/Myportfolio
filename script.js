4e7df033f5412e601ab002ed8f4c4ed78e982413
Abdoulaye2099 <abdoulaye.ba45@unchk.edu.sn>
2026-04-24 11:40:15 -1100

    again

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["développeur web orienté digital."],
+    strings : ["développeur web orienté digital et IA."],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
